ba00572121cc8f5eedb671dc6e11d511755cfa13
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-05-17 11:04:32 -0400

    initial commit

---
+++ Fit4Life_WebApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fit4Life_WebApp.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fit4Life_WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Fit4Life_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fit4Life_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
